f2bed58d803bc49e8d789a5056674d158ce250d6
cfj <3182779524@qq.com>
2018-11-22 08:59:03 +0800

    作业的第一次提交

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,71 @@
 import { connect } from 'react-redux';
 import HomeUI from './HomeUI';
+import React,{Component} from 'react';
+import axios from 'axios'
+import { Result } from 'antd-mobile';
+
+const proxy = "https://bird.ioliu.cn/v1/?url=";
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      entries:[],
+      banner:[],
+      restaurant:[],
+      position:{}
+    }
+  }
+
+  render() {
+    return (<HomeUI {...this.state} />)
+  }
+  componentDidMount() {
+    axios.get(proxy +'https://h5.ele.me/restapi/shopping/v2/entries?latitude=22.533719&longitude=113.936091&templates[]=main_template&templates[]=favourable_template&templates[]=svip_template&terminal=h5')
+      .then(result =>{
+        // console.log(result)
+        this.setState({
+          entries: result.data[0].entries
+        })
+      })
+
+    axios.get(proxy + 'https://h5.ele.me/restapi/shopping/v2/banners?consumer=1&type=1&latitude=22.533719&longitude=113.936091')
+      .then(result =>{
+        // console.log(result)
+        this.setState({
+          banner: result.data
+        })
+      })
+
+      //https://h5.ele.me/restapi/shopping/v3/restaurants?latitude=22.533719&longitude=113.936091&offset=0&limit=8&extras[]=activities&extras[]=tags&extra_filters=home&rank_id=&terminal=h5
+    axios.get(proxy +'https://h5.ele.me/restapi/shopping/v3/restaurants?latitude=22.533719&longitude=113.936091&offset=0&limit=8&extras[]=activities&extras[]=tags&extra_filters=home&rank_id=&terminal=h5')
+      .then(result =>{
+        // console.log(result)
+        this.setState({
+          restaurant:result.data.items
+        })
+      })
+    //https://bird.ioliu.cn/v2/?url=https://apis.map.qq.com/ws/location/v1/ip?ip=113.92.93.53&key=TKUBZ-D24AF-GJ4JY-JDVM2-IBYKK-KEBCU
+    axios.get('https://bird.ioliu.cn/ip')
+      .then(result => {
+        var ip = result.data.data.ip
+        axios.get(`https://bird.ioliu.cn/v2/?url=https://apis.map.qq.com/ws/location/v1/ip?ip=${ip}&key=TKUBZ-D24AF-GJ4JY-JDVM2-IBYKK-KEBCU`).then(result =>{
+          this.setState({
+            position:result.data.result
+          })
+        })
+      })
+
+  }
+}
 
 const mapStateToProps = ({ Home }) => {
-  return {}
+  return {
+    data: Home.data
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(HomeUI);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/pages/Home/HomeUI.js
+++ src/pages/Home/HomeUI.js
@@ -1,18 +1,108 @@
 // 这个是 home ui 组件
 import React from 'react';
+// import { Injector } from 'ts-di';
+// import { Injector } from '/path/to/node_modules/ts-di/dist/index.js';
+// import { Text, View } from 'react-native';
 import './Home.scss';
+import { StickyContainer, Sticky } from 'react-sticky';
+import { SearchBar, Grid, WingBlank, Carousel, Tabs, WhiteSpace, Flex } from 'antd-mobile';
 import Footer from '../../components/Footer/index';
 
-
 const HomeUI = (props) => {
+  const data = props.entries.map((item, index) => ({
+    icon: `https://fuss10.elemecdn.com/${item.image_hash}.jpeg`,
+    text: item.name,
+  }));
+  setTimeout(() => {
+    console.log(props.position)
+  }, 0);
   return (
     <div className="home">
       <div className="elm-home">
-        这是首页
+          <div className="cfj-header">
+            <div className="cfj-adress">{props.position.ad_info && props.position.ad_info.district}</div>
+          </div>
+          <div className="cfj-seach"><SearchBar placeholder="搜索饿了么商家，商品名称" /></div>
+          <div className="cfj-home-activity">
+            <Grid data={data && data} isCarousel dotStyle={{ "width": 10, "height": 3 }} dotActiveStyle={{ "width": 10, "height": 3, "background": "blue" }} infinite hasLine={false} columnNum={5} />
+          </div>
+            {/* <Sticky>
+              {({ style,wasSticky}) =>(
+                <SearchBar style={{...style,background:"#0085ff",paddingBottom:6}} placeholder="搜索饿了么商家，商品名称" />
+              )}
+            </Sticky> */}
+          <div className="cfj-banner">
+            <WingBlank style={{"touchAction":"none"}}>
+              <Carousel autoplay={true} autoplayInterval={1000} infinite dotStyle={{ "width": 10, "height": 3 }} dotActiveStyle={{ "width": 10, "height": 3, "background": "blue" }}>
+            {/* <Carousel autoplay infinite autoplayInterval={1000}> */}
+                {props.banner.map((item,index) => (
+                    <img
+                      key={index}
+                      src={`https://fuss10.elemecdn.com/${item.image_hash}.jpeg`}
+                      alt=""
+                      style={{ width: '100%', verticalAlign: 'top' }}
+                      onLoad={() => {
+                        // fire window resize event to change height
+                        window.dispatchEvent(new Event('resize'));
+                      }}
+                    />
+                ))}
+              </Carousel>
+            </WingBlank>
+          </div>
+
+        <StickyContainer>
+          <div className="cfj-recommend">
+            <div className="cfj-recommend-tit">推荐商家</div>
+            <Sticky>
+              {({ style }) => (
+                <ul style={{ ...style, top: 150, zIndex: 99, height: 40, top: 40,display:"flex",textAlign:"center",justifyContent:"space-around",lineHeight:"40px",background:"#FFF",borderBottom:"1px solid #ddd" }}>
+                  <li>综合排序</li>
+                  <li>距离最近</li>
+                  <li>品质联盟</li>
+                  <li>筛选</li>
+                </ul>
+              )}
+              {/* <div className="cfj-recommend-title">
+              </div> */}
+            </Sticky>
+            <div className="cfj-recommend-list">
+              <ul>
+                {props.restaurant.map((item,index) =>(
+                  <li key={index}>
+                    <div className="shopinfo">
+                      <div className="shopimg">
+                        <img src={`https://fuss10.elemecdn.com/${item.restaurant.image_path}.${/jpeg/.test(item.restaurant.image_path) == true ? 'jpeg' : 'png'}`} alt="" />
+                      </div>
+                      <div className="shopbody">
+                        <h3 className="shopname">{item.restaurant.name}</h3>
+                        <div className="shopnum">评分{item.restaurant.rating}月售{item.restaurant.recent_order_num}单</div>
+                        <div className="shopprice">￥{item.restaurant.piecewise_agent_fee.rules[0].price}起送|{item.restaurant.piecewise_agent_fee.tips}</div>
+                      </div>
+                    </div>
+                    <div className="shopactive">
+                      <div className="shoptag">
+                        <span>{item.restaurant.support_tags[0].text}</span><span>{item.restaurant.support_tags[1] && item.restaurant.support_tags[1].text}</span>
+                      </div>
+                      <div className="nowactive">
+                        <span style={{ "background": `#${item.restaurant.activities[0].icon_color}` }}>{item.restaurant.activities[0].icon_name}</span>
+                        {item.restaurant.activities[0].description}
+                      </div>
+                      <div className="nowactive">
+                        <span style={{ "background": `#${item.restaurant.activities[0].icon_color}`}}>{item.restaurant.activities[1] && item.restaurant.activities[1].icon_name}</span>
+                        {item.restaurant.activities[1] && item.restaurant.activities[1].description}
+                      </div>
+                    </div>
+                  </li>
+                ))}
+
+              </ul>
+            </div>
+          </div>
+        </StickyContainer>
       </div>
       <Footer></Footer>
     </div>
     )
 }
-
 export default HomeUI;
