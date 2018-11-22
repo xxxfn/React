8a81248a829345120f29f12902196e23ee5b4835
xxxfn <15159000743@163.com>
2018-11-22 09:09:17 +0800

    第一次合并前的add .

---
+++ src/pages/DeliciousFood/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const DeliciousFood = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
+export const Elmorder = styled.div`
+  flex: 1;
+`
--- src/pages/Home/HomeUI.js
+++ src/pages/Home/HomeUI.js
@@ -2,13 +2,16 @@
 import React from 'react';
 import './Home.scss';
 import Footer from '../../components/Footer/index';
+import { NavLink } from 'react-router-dom';
 
 
 const HomeUI = (props) => {
   return (
     <div className="home">
       <div className="elm-home">
-        这是首页
+        <p>这是首页</p>
+        <br />
+      <NavLink to="/DeliciousFood">美食节</NavLink>
       </div>
       <Footer></Footer>
     </div>
