1ce085f240446ed0d59234151e7511b45e476090
cfj <3182779524@qq.com>
2018-11-22 21:20:56 +0800

    首页的dierci

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -20,38 +20,58 @@ class Home extends Component {
     return (<HomeUI {...this.state} />)
   }
   componentDidMount() {
-    axios.get(proxy +'https://h5.ele.me/restapi/shopping/v2/entries?latitude=22.533719&longitude=113.936091&templates[]=main_template&templates[]=favourable_template&templates[]=svip_template&terminal=h5')
-      .then(result =>{
-        // console.log(result)
-        this.setState({
-          entries: result.data[0].entries
-        })
-      })
+    // axios.get('https://bird.ioliu.cn/ip')
+    //   .then(result => {
+    //     var lat = result.data.data.location.lat;
+    //     var lng = result.data.data.location.lng;
 
-    axios.get(proxy + 'https://h5.ele.me/restapi/shopping/v2/banners?consumer=1&type=1&latitude=22.533719&longitude=113.936091')
-      .then(result =>{
-        // console.log(result)
-        this.setState({
-          banner: result.data
-        })
-      })
+    //   })
+
+    // axios.get('https://bird.ioliu.cn/ip')
+    //   .then(result => {
+    //     var lat = result.data.data.location.lat;
+    //     var lng = result.data.data.location.lng;
+
+    //   })
 
       //https://h5.ele.me/restapi/shopping/v3/restaurants?latitude=22.533719&longitude=113.936091&offset=0&limit=8&extras[]=activities&extras[]=tags&extra_filters=home&rank_id=&terminal=h5
-    axios.get(proxy +'https://h5.ele.me/restapi/shopping/v3/restaurants?latitude=22.533719&longitude=113.936091&offset=0&limit=8&extras[]=activities&extras[]=tags&extra_filters=home&rank_id=&terminal=h5')
-      .then(result =>{
-        // console.log(result)
-        this.setState({
-          restaurant:result.data.items
-        })
-      })
+    // axios.get('https://bird.ioliu.cn/ip')
+    //   .then(result =>{
+
+    //   })
     //https://bird.ioliu.cn/v2/?url=https://apis.map.qq.com/ws/location/v1/ip?ip=113.92.93.53&key=TKUBZ-D24AF-GJ4JY-JDVM2-IBYKK-KEBCU
     axios.get('https://bird.ioliu.cn/ip')
       .then(result => {
         var ip = result.data.data.ip
         axios.get(`https://bird.ioliu.cn/v2/?url=https://apis.map.qq.com/ws/location/v1/ip?ip=${ip}&key=TKUBZ-D24AF-GJ4JY-JDVM2-IBYKK-KEBCU`).then(result =>{
+
           this.setState({
             position:result.data.result
           })
+          var lat = result.data.result.location.lat;
+          var lng = result.data.result.location.lng;
+          axios.get(proxy + `https://h5.ele.me/restapi/shopping/v3/restaurants?latitude=${lat}&longitude=${lng}&offset=0&limit=8&extras[]=activities&extras[]=tags&extra_filters=home&rank_id=&terminal=h5`)
+            .then(result => {
+              // console.log(result)
+              this.setState({
+                restaurant: result.data.items
+              })
+            })
+          axios.get(proxy + `https://h5.ele.me/restapi/shopping/v2/banners?consumer=1&type=1&latitude=${lat}&longitude=${lng}`)
+            .then(result => {
+              // console.log(result)
+              this.setState({
+                banner: result.data
+              })
+            })
+          axios.get(proxy + `https://h5.ele.me/restapi/shopping/v2/entries?latitude=${lat}&longitude=${lng}&templates[]=main_template&templates[]=favourable_template&templates[]=svip_template&terminal=h5`)
+            .then(result => {
+              // console.log(result)
+              this.setState({
+                entries: result.data[0].entries
+              })
+            })
+
         })
       })
 
--- src/pages/Home/HomeUI.js
+++ src/pages/Home/HomeUI.js
@@ -5,7 +5,7 @@ import React from 'react';
 // import { Text, View } from 'react-native';
 import './Home.scss';
 import { StickyContainer, Sticky } from 'react-sticky';
-import { SearchBar, Grid, WingBlank, Carousel, Tabs, WhiteSpace, Flex } from 'antd-mobile';
+import { SearchBar, Grid, WingBlank, Carousel } from 'antd-mobile';
 import Footer from '../../components/Footer/index';
 import { NavLink } from 'react-router-dom';
 
@@ -21,12 +21,32 @@ const HomeUI = (props) => {
     <div className="home">
       <div className="elm-home">
           <div className="cfj-header">
-            <div className="cfj-adress">{props.position.ad_info && props.position.ad_info.district}</div>
+          <div className="cfj-adress"><i className="iconfont icon-location"></i>{props.position.ad_info && props.position.ad_info.district}</div>
           </div>
           <div className="cfj-seach"><SearchBar placeholder="搜索饿了么商家，商品名称" /></div>
-          <div className="cfj-home-activity">
-            <Grid data={data && data} isCarousel dotStyle={{ "width": 10, "height": 3 }} dotActiveStyle={{ "width": 10, "height": 3, "background": "blue" }} infinite hasLine={false} columnNum={5} />
-          </div>
+          <WingBlank style={{"touchAction":"none"}}>
+            <div className="cfj-home-activity">
+            <Grid data={data}
+              columnNum={5}
+              isCarousel
+              dotStyle={{ "width": 10, "height": 3 }}
+              dotActiveStyle={{ "width": 10, "height": 3, "background": "blue" }}
+              infinite
+              hasLine={false}
+              renderItem={dataItem => (
+                <NavLink to="/DeliciousFood">
+                  <div>
+                    <img src={dataItem.icon} style={{ width: '45px', height: '45px' }} alt="" />
+                    <div style={{ color: '#888', fontSize: '12px', marginTop: '10px' }}>
+                      <span>{dataItem.text}</span>
+                    </div>
+                  </div>
+                </NavLink>
+              )}
+            />
+              {/* <Grid data={data && data} isCarousel dotStyle={{ "width": 10, "height": 3 }} dotActiveStyle={{ "width": 10, "height": 3, "background": "blue" }} infinite hasLine={false} columnNum={5} /> */}
+            </div>
+          </WingBlank>
             {/* <Sticky>
               {({ style,wasSticky}) =>(
                 <SearchBar style={{...style,background:"#0085ff",paddingBottom:6}} placeholder="搜索饿了么商家，商品名称" />
@@ -35,7 +55,7 @@ const HomeUI = (props) => {
           <div className="cfj-banner">
             <WingBlank style={{"touchAction":"none"}}>
               <Carousel autoplay={true} autoplayInterval={1000} infinite dotStyle={{ "width": 10, "height": 3 }} dotActiveStyle={{ "width": 10, "height": 3, "background": "blue" }}>
-            {/* <Carousel autoplay infinite autoplayInterval={1000}> */}
+                {/* <Carousel autoplay infinite autoplayInterval={1000}> */}
                 {props.banner.map((item,index) => (
                     <img
                       key={index}
@@ -51,7 +71,6 @@ const HomeUI = (props) => {
               </Carousel>
             </WingBlank>
           </div>
-
         <StickyContainer>
           <div className="cfj-recommend">
             <div className="cfj-recommend-tit">推荐商家</div>
@@ -61,7 +80,7 @@ const HomeUI = (props) => {
                   <li>综合排序</li>
                   <li>距离最近</li>
                   <li>品质联盟</li>
-                  <li>筛选</li>
+                  <li>筛选<i className="iconfont icon-shaixuan"></i></li>
                 </ul>
               )}
               {/* <div className="cfj-recommend-title">
@@ -70,31 +89,42 @@ const HomeUI = (props) => {
             <div className="cfj-recommend-list">
               <ul>
                 {props.restaurant.map((item,index) =>(
-                  <li key={index}>
-                    <div className="shopinfo">
-                      <div className="shopimg">
-                        <img src={`https://fuss10.elemecdn.com/${item.restaurant.image_path}.${/jpeg/.test(item.restaurant.image_path) == true ? 'jpeg' : 'png'}`} alt="" />
+                  // <NavLink to={`/details/${item.restaurant.id}/0`}>
+                  <NavLink to={{ pathname: `/details/${item.restaurant.id}/0`, search: `?lat=${props.position.location && props.position.location.lat}&lng=${props.position.location && props.position.location.lng}`}}>
+                    <li key={index}>
+                      <div className="shopinfo">
+                        <div className="shopimg">
+                          <img src={`https://fuss10.elemecdn.com/${item.restaurant.image_path}.${/jpeg/.test(item.restaurant.image_path) === true ? 'jpeg' : 'png'}`} alt="" />
+                        </div>
+                        <div className="shopbody">
+                          <h3 className="shopname">{item.restaurant.name}</h3>
+                          <div className="shopnum">
+                            <span style={{"width":item.restaurant.rating*12}}><svg width="60" height="10" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="0%" y1="50%" y2="50%" id="a"><stop stop-color="#FFDE00" offset="0%" /><stop stop-color="#FFB000" offset="100%" /></linearGradient></defs><path d="M54.017 8.072l-2.552 1.561c-.476.291-.758.096-.626-.455l.696-2.909-2.273-1.944c-.424-.362-.325-.691.239-.736l2.982-.237L53.63.589c.213-.515.557-.523.774 0l1.146 2.763 2.982.237c.556.044.67.368.24.736l-2.274 1.944.696 2.91c.13.542-.143.75-.626.454l-2.551-1.56zm-48 0L3.465 9.633c-.476.291-.758.096-.626-.455l.696-2.909-2.273-1.944c-.424-.362-.325-.691.239-.736l2.982-.237L5.63.589c.213-.515.557-.523.774 0L7.55 3.352l2.982.237c.556.044.67.368.24.736L8.497 6.269l.696 2.91c.13.542-.143.75-.626.454l-2.551-1.56zm12 0l-2.552 1.561c-.476.291-.758.096-.626-.455l.696-2.909-2.273-1.944c-.424-.362-.325-.691.239-.736l2.982-.237L17.63.589c.213-.515.557-.523.774 0l1.146 2.763 2.982.237c.556.044.67.368.24.736l-2.274 1.944.696 2.91c.13.542-.143.75-.626.454l-2.551-1.56zm12 0l-2.552 1.561c-.476.291-.758.096-.626-.455l.696-2.909-2.273-1.944c-.424-.362-.325-.691.239-.736l2.982-.237L29.63.589c.213-.515.557-.523.774 0l1.146 2.763 2.982.237c.556.044.67.368.24.736l-2.274 1.944.696 2.91c.13.542-.143.75-.626.454l-2.551-1.56zm12 0l-2.552 1.561c-.476.291-.758.096-.626-.455l.696-2.909-2.273-1.944c-.424-.362-.325-.691.239-.736l2.982-.237L41.63.589c.213-.515.557-.523.774 0l1.146 2.763 2.982.237c.556.044.67.368.24.736l-2.274 1.944.696 2.91c.13.542-.143.75-.626.454l-2.551-1.56z" fill="url(#a)" fill-rule="evenodd" /></svg></span>
+                            <span className="ppp">{item.restaurant.rating}</span>
+                            <span>月售{item.restaurant.recent_order_num}单</span>
+                            <div style={{ "float": "right", "background": `#${item.restaurant.delivery_mode && item.restaurant.delivery_mode.gradient.rgb_to}`,"color":"#fff","marginRight":"15px","padding":"1px"}}>{item.restaurant.delivery_mode && item.restaurant.delivery_mode.text}</div>
+                          </div>
+                          <div className="shopprice">
+                            <div>￥{item.restaurant.piecewise_agent_fee.rules[0].price}起送|{item.restaurant.piecewise_agent_fee.tips}</div>
+                            <div>{(item.restaurant.distance/1000).toFixed(2)}km  |  {item.restaurant.order_lead_time}分钟</div>
+                          </div>
+                        </div>
                       </div>
-                      <div className="shopbody">
-                        <h3 className="shopname">{item.restaurant.name}</h3>
-                        <div className="shopnum">评分{item.restaurant.rating}月售{item.restaurant.recent_order_num}单</div>
-                        <div className="shopprice">￥{item.restaurant.piecewise_agent_fee.rules[0].price}起送|{item.restaurant.piecewise_agent_fee.tips}</div>
+                      <div className="shopactive">
+                        <div className="shoptag">
+                          <span style={{ "border": "1px solid #999" }}>{item.restaurant.support_tags[0].text}</span><span style={{ "border": item.restaurant.support_tags[1] && "1px solid #999"}}>{item.restaurant.support_tags[1] && item.restaurant.support_tags[1].text}</span>
+                        </div>
+                        <div className="nowactive">
+                          <span style={{ "background": `#${item.restaurant.activities[0] && item.restaurant.activities[0].icon_color}` }}>{item.restaurant.activities[0] && item.restaurant.activities[0].icon_name}</span>
+                          {item.restaurant.activities[0] && item.restaurant.activities[0].description}
+                        </div>
+                        <div className="nowactive">
+                          <span style={{ "background": `#${item.restaurant.activities[1] && item.restaurant.activities[1].icon_color}`}}>{item.restaurant.activities[1] && item.restaurant.activities[1].icon_name}</span>
+                          {item.restaurant.activities[1] && item.restaurant.activities[1].description}
+                        </div>
                       </div>
-                    </div>
-                    <div className="shopactive">
-                      <div className="shoptag">
-                        <span>{item.restaurant.support_tags[0].text}</span><span>{item.restaurant.support_tags[1] && item.restaurant.support_tags[1].text}</span>
-                      </div>
-                      <div className="nowactive">
-                        <span style={{ "background": `#${item.restaurant.activities[0].icon_color}` }}>{item.restaurant.activities[0].icon_name}</span>
-                        {item.restaurant.activities[0].description}
-                      </div>
-                      <div className="nowactive">
-                        <span style={{ "background": `#${item.restaurant.activities[0].icon_color}`}}>{item.restaurant.activities[1] && item.restaurant.activities[1].icon_name}</span>
-                        {item.restaurant.activities[1] && item.restaurant.activities[1].description}
-                      </div>
-                    </div>
-                  </li>
+                    </li>
+                  </NavLink>
                 ))}
 
               </ul>
